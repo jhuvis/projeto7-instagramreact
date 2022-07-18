771615896610b208397148543c40be31adb5378d
unknown <jose.filipe.toledo.1999@gmail.com>
2022-07-18 17:00:37 -0300

    esqueci do app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
 import ReactDOM from "react-dom";
-import Navbar from "./Navbar";
-import Corpo from "./Corpo";
-import FundoMobile from "./FundoMobile";
-
-function App() {
-    return (
-        <div>
-            <Navbar />
-            <Corpo />
-            <FundoMobile /> 
-        </div>
-        
-    );
-  }
-  
-  export default App;
+import App from "./App";
 
 ReactDOM.render(<App />, document.querySelector(".root"));
